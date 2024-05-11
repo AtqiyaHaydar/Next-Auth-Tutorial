aad5d7d2fc37865df91e1fc48201081db34a0ac5
AtqiyaHaydar <ashford1524@gmail.com>
2024-05-11 17:50:50 +0700

    feat: session callback

--- src/auth.ts
+++ src/auth.ts
@@ -7,5 +7,11 @@ import { Adapter } from "next-auth/adapters"
 export const { handlers, signIn, signOut, auth } = NextAuth({
   trustHost: true,
   adapter: PrismaAdapter(prisma) as Adapter,
+  callbacks: {
+    session({session, user}) {
+      session.user.role = user.role
+      return session;
+    }
+  },
   providers: [Google],
 })
