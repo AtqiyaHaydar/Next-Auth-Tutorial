9378065179fae5e9a1a8243c6da92a952c060b5b
AtqiyaHaydar <ashford1524@gmail.com>
2024-05-11 17:45:45 +0700

    feat: client side authentication

--- src/auth.ts
+++ src/auth.ts
@@ -2,8 +2,10 @@ import NextAuth from "next-auth"
 import { PrismaAdapter } from "@auth/prisma-adapter" 
 import prisma from "./lib/prisma"
 import Google from "next-auth/providers/google"
+import { Adapter } from "next-auth/adapters"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  adapter: PrismaAdapter(prisma),
+  trustHost: true,
+  adapter: PrismaAdapter(prisma) as Adapter,
   providers: [Google],
 })
--- src/components/UserButton.tsx
+++ src/components/UserButton.tsx
@@ -13,7 +13,7 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "./ui/dropdown-menu";
-import { signOut } from "@/auth";
+import { signOut } from "next-auth/react";
 
 interface UserButtonProps {
   user: User;
@@ -53,14 +53,11 @@ export default function UserButton({ user }: UserButtonProps) {
         </DropdownMenuGroup>
         <DropdownMenuSeparator />
         <DropdownMenuItem asChild>
-          <form action={async () => {
-            "use server";
-            await signOut();
-          }}>
-              <button type="submit" className="flex w-full items-center">
-                <LogOut className="mr-2 h-4 w-4" /> Sign Out
-              </button>
-          </form>
+          <button onClick={() => signOut({
+            callbackUrl: "/",
+          })} className="flex w-full items-center">
+            <LogOut className="mr-2 h-4 w-4" /> Sign Out
+          </button>
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
